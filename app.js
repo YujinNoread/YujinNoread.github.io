d00f138eb2108ba6e71914debcb8106b0c176ac3
Evgen <yujinwork19@gmail.com>
2020-08-06 20:04:58 +0300

    add more details functional modal

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* addToCart */
 let productsCount = document.getElementById("product-count");
 let addToCartButtons = document.querySelectorAll(".body__add");
 for(let i=0;i<addToCartButtons.length;i++){
@@ -6,7 +7,7 @@ for(let i=0;i<addToCartButtons.length;i++){
 	});
 }
 
-/* */
+/* liked */
 
 let likeButtons = document.querySelectorAll(".image__like");
 for(let j=0;j<likeButtons.length;j++){
@@ -15,8 +16,39 @@ for(let j=0;j<likeButtons.length;j++){
 	});
 }
 
-/* */
+/* slider*/
 
 $('.slider-block').slick({
 	dots:true,
 });
+
+/* More details */
+
+let moreDetailsButtons = document.querySelectorAll(".body__details");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".close-btn");
+
+function openModal(){
+	modal.classList.add("show");
+	modal.classList.remove("hide");
+}
+
+function closeModal(){
+	modal.classList.remove("show");
+	modal.classList.add("hide");
+}
+
+moreDetailsButtons.forEach(function(btn){
+	btn.addEventListener("click", openModal)
+});
+
+closeBtn.addEventListener("click",closeModal);
+
+modal.addEventListener("click", function(event){
+	if(event.target === modal){
+		closeModal();
+	}
+})
+
+
+
